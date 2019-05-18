c1d032df2da9fdc27b878b667fc3f6838c2d96f9
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-18 12:03:56 +0100

    poprawione

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,26 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner sk = new Scanner(System.in);
+        System.out.println("Podaj pierwszy wyraz ciągu:");
+        int a = sk.nextInt();
+        System.out.println("Podaj dlugosc ciagu: ");
+        int n = sk.nextInt();
+        System.out.println("Podaj przyrost:");
+        int d = sk.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            int next = a + (i - 1) * d;
+            System.out.println(next);
+
+        }
+
+
     }
 }
